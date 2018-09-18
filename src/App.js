0a0109930a0a565f965d39d4f77c2a58f28d255a
Ikhalas <5910110405@psu.ac.th>
2018-09-19 00:04:12 +0700

    add WordCard.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 import logo from './logo.svg';
 import './App.css';
 
-
-const word = "Hello";
 class App extends Component {
   render() {
     return (
       <div>
         {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+          <WordCard value="hello"/>
         }
  </div>
 
---
+++ src/WordCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+
+export default class WordCard extends Component {
+render() {
+ return (
+ <div>
+{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+ </div>
+ );
+ }
+}
