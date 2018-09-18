dc9d3621d79c95d7cd5e482084073067b474b3c3
Ikhalas <5910110405@psu.ac.th>
2018-09-18 22:12:39 +0700

    Character (h,i)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
+      <div>
+        <CharacterCard value="h"/>
+        <CharacterCard value="i"/>
       </div>
+
     );
   }
 }
---
+++ src/CharacterCard.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export default class CharacterCard extends Component {
+    render(){
+        return (
+            <div>{this.props.value}</div>
+        )
+    }
+   }
