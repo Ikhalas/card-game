90c85679cec299fa8a71ecd3175fa04d3954877c
Ikhalas <5910110405@psu.ac.th>
2018-09-18 23:39:01 +0700

    add CSS

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 export default class CharacterCard extends Component {
     render(){
         return (
-            <div>{this.props.value}</div>
+            <div className = "card">{this.props.value}</div>
         )
     }
    }
