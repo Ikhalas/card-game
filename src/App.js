e097feced491309f23399aec620f32706e00bca8
Ikhalas <5910110405@psu.ac.th>
2018-09-18 22:19:37 +0700

    Word  "Hello"

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import CharacterCard from './CharacterCard';
 import logo from './logo.svg';
 import './App.css';
 
+
+const word = "Hello";
 class App extends Component {
   render() {
     return (
       <div>
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
-      </div>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        }
+ </div>
+
 
     );
   }
